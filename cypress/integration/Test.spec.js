bbbf9e91257bcc518bde964a4ad6dce4b8d6f4f6
dvoznyi <dvoznyi17@gmail.com>
2021-07-12 10:15:31 +0300

    123

---
+++ cypress/integration/Test.spec.js
@@ -0,0 +1,17 @@
+import config from "../fixtures/config.json"
+import pet from "../fixtures/petPayload.json"
+
+it('dvTest', () => {
+    pet.name = "Katya";
+    pet.tags[0].name  = "firstTag"
+    pet.tags[0].name  = "secondTag"
+    pet.status = "unavailable"
+    cy.request('POST', `${config.baseURL}/pet`, pet).then((response) => {
+        let textResp = JSON.stringify(response.body)
+        console.log(textResp)
+        cy.log(textResp)
+        expect(response.body).to.have.property('name', 'Katya') // true 
+        expect(response.body).to.have.property('status', 'unavailable')
+        //expect(response.body).to.have.property('tags[0].name', 'firstTag') //не получается =)
+    })
+    })
