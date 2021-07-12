6c0ea6ed4a94ae7dcec12ca736e040b1b5a900a7
dvoznyi <dvoznyi17@gmail.com>
2021-07-12 12:25:15 +0300

    1488

--- cypress/integration/Test.spec.js
+++ cypress/integration/Test.spec.js
@@ -4,14 +4,14 @@ import pet from "../fixtures/petPayload.json"
 it('dvTest', () => {
     pet.name = "Katya";
     pet.tags[0].name  = "firstTag"
-    pet.tags[0].name  = "secondTag"
+    pet.tags[0].name  = "secondTag" //теги
     pet.status = "unavailable"
     cy.request('POST', `${config.baseURL}/pet`, pet).then((response) => {
         let textResp = JSON.stringify(response.body)
         console.log(textResp)
         cy.log(textResp)
         expect(response.body).to.have.property('name', 'Katya') // true 
-        expect(response.body).to.have.property('status', 'unavailable')
-        //expect(response.body).to.have.property('tags[0].name', 'firstTag') //не получается =)
+        expect(response.body).to.have.property('status', 'unavailable') //проверки шо  я сделал
+        //expect(response.body).to.have.property('tags[0].name', 'firstTag') //не получается
     })
     })
