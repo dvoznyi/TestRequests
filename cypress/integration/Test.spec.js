9241cd167484b1f0aa24ec9bf6a21b2ad4ec35a6
dvoznyi <dvoznyi17@gmail.com>
2021-07-25 15:17:15 +0300

    4834823

--- cypress/integration/Test.spec.js
+++ cypress/integration/Test.spec.js
@@ -3,8 +3,9 @@ import pet from "../fixtures/petPayload.json"
 
 it('dvTest', () => {
     pet.name = "Katya";
+    pet.id = "45678937894"
     pet.tags[0].name  = "firstTag"
-    pet.tags[0].name  = "secondTag" //теги
+    pet.tags[1].name  = "secondTag" //теги
     pet.status = "unavailable"
     cy.request('POST', `${config.baseURL}/pet`, pet).then((response) => {
         let textResp = JSON.stringify(response.body)
@@ -12,12 +13,56 @@ it('dvTest', () => {
         cy.log(textResp)
         expect(response.body).to.have.property('name', 'Katya') // true 
         expect(response.body).to.have.property('status', 'unavailable');
-        //expect(response.body).to.have.property('tags', 'firstTag') //не получается
+        //expect(response.body).to.have.property('tags', 'firstTag')
+        //expect(response.body).to.have.property('tags', 'secondTag')
         expect(response).property('status').to.equal(200);
         expect(response.body).property('id').to.not.be.oneOf([null, ""]);
            let petid = response.body.id
-           cy.request('GET', `${config.baseURL}/pet`) // тут не правильный эндпоинт, посмотри в документации
-                                                    //тебе нужно сделать такую же конструкцию как в 9 строке: cy.request(...).then((response) => {}
-
-    })
+           cy.wait(2000)
+           cy.request(`${config.baseURL}/pet/${petid}`).then((response) => {
+            expect(response.body).to.have.property('name', 'Katya') // true 
+            expect(response.body).to.have.property('status', 'unavailable');
+            //expect(response.body).to.have.property('tags', 'firstTag')
+            expect(response).property('status').to.equal(200);
+            expect(response.body).property('id').to.not.be.oneOf([null, ""]);
+            cy.request('PUT', `${config.baseURL}/pet/`,
+            {
+                "id": 45678937894,
+                "category": {
+                  "id": 0,
+                  "name": "string"
+                },
+                "name": "Dima",
+                "photoUrls": [
+                  "string"
+                ],
+                "tags": [
+                  {
+                    "id": 0,
+                    "name": "string"
+                  },
+                       {
+                    "id": 1,
+                    "name": "string"
+                  }
+              
+                ],
+                "status": "available"
+              }).then((response) => {
+                expect(response.body).to.have.property('name', 'Dima') // true 
+                expect(response.body).to.have.property('status', 'available');
+                expect(response).property('status').to.equal(200);
+                expect(response.body).property('id').to.not.be.oneOf([null, ""]);
+                cy.request(`${config.baseURL}/pet/${petid}`).then((response) => {
+                expect(response.body).to.have.property('name', 'Dima') // true 
+                expect(response.body).to.have.property('status', 'available');
+                //expect(response.body).to.have.property('tags', 'firstTag')
+                expect(response).property('status').to.equal(200);
+                expect(response.body).property('id').to.not.be.oneOf([null, ""]);
+                })
+            })
+            
+           })
+        })
+           
     })
