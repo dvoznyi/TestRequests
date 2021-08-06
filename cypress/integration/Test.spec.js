28b34d641b6394e31ad8a74925063d7699b7b08e
dvoznyi <dvoznyi17@gmail.com>
2021-08-06 10:08:26 +0300

    test

--- cypress/integration/Test.spec.js
+++ cypress/integration/Test.spec.js
@@ -1,5 +1,6 @@
 import config from "../fixtures/config.json"
 import pet from "../fixtures/petPayload.json"
+import  putPet from "../fixtures/putPet.json"
 
 it('dvTest', () => {
     pet.name = "Katya";
@@ -8,41 +9,43 @@ it('dvTest', () => {
     pet.tags[1].name  = "secondTag"
     pet.status = "unavailable"
     cy.request('POST', `${config.baseURL}/pet`, pet).then((response) => {
-        let textResp = JSON.stringify(response.body)
-        console.log(textResp)
-        cy.log(textResp)
-        expect(response.body).to.have.property('name', 'Katya') // true 
+        // let textResp = JSON.stringify(response.body)
+      // console.log(textResp)
+        // cy.log(textResp)
+        expect(response.body).to.have.property('name', 'Katya') 
         expect(response.body).to.have.property('status', 'unavailable');
-        //expect(response.body).to.have.property(tags[0].name, 'firstTag') // json path указан не с начала, должно быть pet. ...
-        //expect(response.body).to.have.property('tags', 'secondTag') // вверху же почти проверка на наличие тега ))
-         expect(response).property('status').to.equal(200); //не рабочая проверка
-         expect(response).property('id').to.not.be.oneOf([null, ""]); //айди не может быть пустым, но проверка неплохая, надо сделать так чтоб заработала
+        expect(response.body.tags[0].name).to.equal('firstTag') 
+        expect(response.body.tags[1].name).to.equal('secondTag') 
+         expect(response.status).to.equal(200); 
+         expect(response.body.id).to.not.be.oneOf([null, ""]);
            let petid = response.body.id
-           //cy.wait(2000)
+           cy.wait(2000)
            cy.request(`${config.baseURL}/pet/${petid}`).then((response) => {
             expect(response.body).to.have.property('name', 'Katya')
             expect(response.body).to.have.property('status', 'unavailable');
-            //expect(response.body).to.have.property('tags', 'firstTag')
-            expect(response).property('status').to.equal(200); // не рабочая проверка, исправляй)
+            expect(response.body.tags[0].name).to.equal('firstTag') 
+            expect(response.body.tags[1].name).to.equal('secondTag') 
+            expect(response.status).to.equal(200); 
             expect(response.body).property('id').to.not.be.oneOf([null, ""]);
-            cy.request('PUT',`${config.baseURL}/pet`, putPet).then((response) => { // putPet не определён, нужно его импортировать
-                expect(response.body).to.have.property('name', 'Dima') // true
-                expect(response.body).to.have.property('status', 'available');
-                expect(response).property('status').to.equal(200); // эта проверка после импорта putPet не заработает
-                expect(response.body).property('id').to.not.be.oneOf([null, ""]); // эта тоже, загугли доку сайпресс и посмотри там ассерты
+            cy.request('PUT',`${config.baseURL}/pet`, putPet).then((response) => { 
+                expect(response.body.name).to.equal('Dima') // true
+                expect(response.body.status).to.equal('available');
+                expect(response.status).to.equal(200); 
+                expect(response.body.id).to.not.be.oneOf([null, ""]);
+                cy.wait(2000)
             cy.request(`${config.baseURL}/pet/${petid}`).then((response) => {
-                // expect(response.body).to.have.property('name', 'Dima') // true
-                // expect(response.body).to.have.property('status', 'available');
-                //expect(response.body).to.have.property('tags', 'firstTag')
-                expect(response).property('status').to.equal(200);
-                expect(response.body).property('id').to.not.be.oneOf([null, ""]);
+              expect(response.body.name).to.equal('Katya') // true
+              expect(response.body.status).to.equal('unavailable');
+              expect(response.body.tags[0].name).to.equal('firstTag') 
+              expect(response.body.tags[1].name).to.equal('secondTag') 
+              expect(response.status).to.equal(200);
+              expect(response.body.id).to.not.be.oneOf([null, ""]);
               cy.request('DELETE',`${config.baseURL}/pet/${petid}`).then((response) => {
-                expect(response).property('status').to.equal(200);
+                expect(response.status).to.equal(200);
                 cy.wait(2000)
                 cy.request(`${config.baseURL}/pet/${petid}`).then((response) => {
-                  expect(response).property('status').to.equal(200);
-                  console.log(textResp)
-                  cy.log(textResp)
+                  expect(response.status).to.equal(200);
+                  
                 })
               })
                 })
